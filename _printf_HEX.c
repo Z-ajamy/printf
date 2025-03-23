0e503c530fb53d58362d2e544d320e6808c1bdb9
Z-ajamy <abdorahman0283@gmail.com>
2025-03-23 06:32:56 +0200

    Betty style ;)

--- _printf_HEX.c
+++ _printf_HEX.c
@@ -1,32 +1,52 @@
 #include "main.h"
 
-int _printf_HEX(va_list *list)
-{
-    unsigned int num;
-    int n =0;
-
-    num = va_arg(*list, unsigned int);
-
-    _printf_HEX_rec(&n, num);
-    return n;
-}
-
+/**
+ * _printf_HEX_rec - Recursively prints the uppercase hexadecimal representation of a number
+ * @n: Pointer to an integer that keeps track of the number of printed digits
+ * @num: The unsigned integer to be converted and printed in uppercase hexadecimal
+ *
+ * Description: This function recursively divides the number by 16
+ * to extract hexadecimal digits in the correct order.
+ */
 void _printf_HEX_rec(int *n, unsigned int num)
 {
-    int a;
-    if (num / 16)
-    {
-        _printf_HEX_rec(n,num / 16);
-    }
-    a= num%16;
-    if (a >= 0 && a<= 9)
-    {
-        _putchar('0' + a);
-        *n = *n +1;
-    }
-    else
-    {
-        _putchar('A' + (a-10));
-        *n = *n +1;
-    }
+	int a;
+
+	if (num / 16)
+	{
+		_printf_HEX_rec(n, num / 16);
+	}
+
+	a = num % 16;
+
+	if (a >= 0 && a <= 9)
+	{
+		_putchar('0' + a);
+	}
+	else
+	{
+		_putchar('A' + (a - 10));
+	}
+
+	*n = *n + 1;
+}
+
+/**
+ * _printf_HEX - Converts an unsigned integer to uppercase hexadecimal and prints it
+ * @list: Argument list containing the number to convert
+ *
+ * Description: Extracts an unsigned integer from the argument list and
+ * prints its uppercase hexadecimal representation using recursion.
+ *
+ * Return: The total number of characters printed.
+ */
+int _printf_HEX(va_list *list)
+{
+	unsigned int num;
+	int n = 0;
+
+	num = va_arg(*list, unsigned int);
+
+	_printf_HEX_rec(&n, num);
+	return (n);
 }
--- _printf_oct.c
+++ _printf_oct.c
@@ -1,20 +1,38 @@
 #include "main.h"
 
-int _printf_oct(va_list *list)
-{
-    int n = 0;
-    unsigned int num = va_arg(*list, unsigned int);
-
-    _printf_oct_rec(&n, num);
-    return n;
-}
-
+/**
+ * _printf_oct_rec - Recursively prints the octal representation of a number
+ * @n: Pointer to an integer that keeps track of the number of printed digits
+ * @num: The unsigned integer to be converted and printed in octal
+ *
+ * Description: This function recursively divides the number by 8
+ * to extract octal digits in the correct order.
+ */
 void _printf_oct_rec(int *n, unsigned int num)
 {
-    if (num / 8)
-    {
-        _printf_oct_rec(n, num/8);
-    }
-    _putchar('0' + num %8);
-    *n = *n + 1;
+	if (num / 8)
+	{
+		_printf_oct_rec(n, num / 8);
+	}
+
+	_putchar('0' + num % 8);
+	*n = *n + 1;
+}
+
+/**
+ * _printf_oct - Converts an unsigned integer to octal and prints it
+ * @list: Argument list containing the number to convert
+ *
+ * Description: Extracts an unsigned integer from the argument list and
+ * prints its octal representation using recursion.
+ *
+ * Return: The total number of characters printed.
+ */
+int _printf_oct(va_list *list)
+{
+	int n = 0;
+	unsigned int num = va_arg(*list, unsigned int);
+
+	_printf_oct_rec(&n, num);
+	return (n);
 }
