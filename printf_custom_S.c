3c48f20c7618b91d36c0cc1b68a312e8a0372b13
Z-ajamy <abdorahman0283@gmail.com>
2025-03-24 17:29:01 +0200

    ?

--- printf_custom_S.c
+++ printf_custom_S.c
@@ -3,7 +3,7 @@
 int printf_custom_S(va_list *list, char *p, int *k)
 {
     char *ptr = NULL;
-        int n = 0, len = 0;
+        int n = 0, x = 0;
 
         ptr = va_arg(*list, char *);
         if (ptr == NULL)
@@ -15,19 +15,26 @@ int printf_custom_S(va_list *list, char *p, int *k)
             {
                 Buffer_editor(p, k, '\\');
                 Buffer_editor(p, k, 'x');
+                x += 2;
                 if (!((int)ptr[n] / 16))
                 {
-                Buffer_editor(p, k, '0');
+                    p[*k] = ('0');
+                    *k = *k + 1;
+                    if (*k == 1024)
+                    {
+                        _putchar(p, k);
+                        *k = 0;
+                    }
+                    x++;
                 }
-                _printf_HEX_rec(&len, (unsigned int)ptr[n], p, k);
-                len += 2;
+                _printf_HEX_rec(&x, (unsigned int)ptr[n], p, k);
             }
             else
             {
                 Buffer_editor(p, k, (ptr[n]));
-                len++;
             }
                 n++;
         }
-        return (len);
+
+        return (n + x);
 }
