b414bfa6e813902f4d06b417bdd16f3c5214d89d
Z-ajamy <abdorahman0283@gmail.com>
2025-03-24 22:49:00 +0200

    betty

--- _printf_char.c
+++ _printf_char.c
@@ -1,10 +1,17 @@
 #include "main.h"
 
 /**
- * _printf_char - Prints a single character.
- * @list: The argument list containing the character.
+ * _printf_char - Prints a character from the argument list.
+ * @list: The argument list containing the character to be printed.
+ * @ptr: Pointer to the buffer where the character will be stored.
+ * @k: Pointer to the index of the buffer.
  *
- * Return: Always returns 1 (one character printed).
+ * Description:
+ * This function extracts a character from the variable argument list and
+ * stores it in the buffer using the Buffer_editor function. It returns 1
+ * since only one character is printed.
+ *
+ * Return: The number of characters printed (always 1).
  */
 int _printf_char(va_list *list, char *ptr, int *k)
 {
--- _printf_hex.c
+++ _printf_hex.c
@@ -1,5 +1,16 @@
 #include "main.h"
 
+/**
+ * _printf_hex_rec - Recursively converts and prints an unsigned integer
+ *                   in lowercase hexadecimal format.
+ * @n: Pointer to an integer tracking the number of characters printed.
+ * @num: The unsigned integer to be converted and printed.
+ * @ptr: Pointer to the buffer where formatted output is stored.
+ * @k: Pointer to the index of the buffer.
+ *
+ * Description: This function recursively divides the number by 16
+ *              to extract hexadecimal digits in the correct order.
+ */
 void _printf_hex_rec(int *n, unsigned int num, char *ptr, int *k)
 {
 	int a;
@@ -23,6 +34,18 @@ void _printf_hex_rec(int *n, unsigned int num, char *ptr, int *k)
 	*n = *n + 1;
 }
 
+/**
+ * _printf_hex - Converts an unsigned integer to lowercase hexadecimal
+ *               and prints it.
+ * @list: Argument list containing the number to convert.
+ * @ptr: Pointer to the buffer where formatted output is stored.
+ * @k: Pointer to the index of the buffer.
+ *
+ * Description: Extracts an unsigned integer from the argument list and
+ *              prints its lowercase hexadecimal representation using recursion.
+ *
+ * Return: The total number of characters printed.
+ */
 int _printf_hex(va_list *list, char *ptr, int *k)
 {
 	unsigned int num;
